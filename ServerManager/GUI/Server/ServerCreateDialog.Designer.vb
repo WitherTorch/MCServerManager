f47d93c325a802fe4825b28bdef5e549593f2d79
Error 404 <new1271@yahoo.com.tw>
2019-06-03 17:25:11 +0800

    Add DropDownControls.dll Reference

--- ServerManager/GUI/Server/ServerCreateDialog.Designer.vb
+++ ServerManager/GUI/Server/ServerCreateDialog.Designer.vb
@@ -258,12 +258,12 @@ Partial Class ServerCreateDialog
         '
         Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.將連接埠設成預設值ToolStripMenuItem})
         Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(183, 26)
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(185, 26)
         '
         '將連接埠設成預設值ToolStripMenuItem
         '
         Me.將連接埠設成預設值ToolStripMenuItem.Name = "將連接埠設成預設值ToolStripMenuItem"
-        Me.將連接埠設成預設值ToolStripMenuItem.Size = New System.Drawing.Size(182, 22)
+        Me.將連接埠設成預設值ToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
         Me.將連接埠設成預設值ToolStripMenuItem.Text = "將連接埠設成預設值"
         '
         'Label2
@@ -313,7 +313,7 @@ Partial Class ServerCreateDialog
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(501, 383)
+        Me.TabPage2.Size = New System.Drawing.Size(501, 388)
         Me.TabPage2.TabIndex = 1
         Me.TabPage2.Text = "進階設定"
         Me.TabPage2.UseVisualStyleBackColor = True
@@ -325,7 +325,7 @@ Partial Class ServerCreateDialog
         Me.AdvancedPropertyGrid.Location = New System.Drawing.Point(3, 3)
         Me.AdvancedPropertyGrid.Margin = New System.Windows.Forms.Padding(0)
         Me.AdvancedPropertyGrid.Name = "AdvancedPropertyGrid"
-        Me.AdvancedPropertyGrid.Size = New System.Drawing.Size(495, 377)
+        Me.AdvancedPropertyGrid.Size = New System.Drawing.Size(495, 382)
         Me.AdvancedPropertyGrid.TabIndex = 1
         '
         'TableLayoutPanel1
