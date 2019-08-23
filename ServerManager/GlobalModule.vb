dbc2b5b65a751d7248f1dc4010f0360764bea39b
Error 404 <new1271@yahoo.com.tw>
2019-08-23 16:10:56 +0800

    Release 1.7 Alpha 4

--- ServerManager/GlobalModule.vb
+++ ServerManager/GlobalModule.vb
@@ -6,7 +6,7 @@ Imports ServerManager
 
 Module GlobalModule
     Friend TestForm As ServerCheckingForm
-    Public Const SERVER_MANAGER_VER As String = "1.7 Alpha 3a"
+    Public Const SERVER_MANAGER_VER As String = "1.7 Alpha 3b"
     Friend Manager As Manager
     Friend IsUnixLikeSystem As Boolean
     Friend RunningBungeeCord As Boolean = False
