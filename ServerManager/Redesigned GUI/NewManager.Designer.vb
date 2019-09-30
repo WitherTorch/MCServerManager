c0063ddcaaca4fa6e713f0a41a55a30ca3ede104
Error 404 <new1271@yahoo.com.tw>
2019-10-01 03:10:58 +0800

    .

--- ServerManager/Redesigned GUI/NewManager.Designer.vb
+++ ServerManager/Redesigned GUI/NewManager.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class NewManager
     Inherits MetroFramework.Forms.MetroForm
 
     'Form 覆寫 Dispose 以清除元件清單。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class NewManager
     '注意: 以下為 Windows Form 設計工具所需的程序
     '可以使用 Windows Form 設計工具進行修改。
     '請勿使用程式碼編輯器進行修改。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.StyleManager = New MetroFramework.Components.MetroStyleManager(Me.components)
@@ -28,12 +28,13 @@ Partial Class NewManager
         Me.MetroTile2 = New MetroFramework.Controls.MetroTile()
         Me.MetroTile1 = New MetroFramework.Controls.MetroTile()
         Me.CPUCircularBar = New CircularProgressBar.CircularProgressBar()
-        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.CPUPerformanceCounter = New System.Diagnostics.PerformanceCounter()
         Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.NetworkCircularBar = New CircularProgressBar.CircularProgressBar()
+        Me.RAMCircularBar = New CircularProgressBar.CircularProgressBar()
+        Me.VRAMCircularBar = New CircularProgressBar.CircularProgressBar()
         CType(Me.StyleManager, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TableLayoutPanel1.SuspendLayout()
-        Me.Panel1.SuspendLayout()
         CType(Me.CPUPerformanceCounter, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -91,41 +92,32 @@ Partial Class NewManager
         Me.CPUCircularBar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner
         Me.CPUCircularBar.AnimationSpeed = 500
         Me.CPUCircularBar.BackColor = System.Drawing.Color.Transparent
-        Me.CPUCircularBar.Dock = System.Windows.Forms.DockStyle.Fill
         Me.CPUCircularBar.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold)
         Me.CPUCircularBar.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.CPUCircularBar.InnerColor = System.Drawing.Color.WhiteSmoke
         Me.CPUCircularBar.InnerMargin = 2
         Me.CPUCircularBar.InnerWidth = -1
-        Me.CPUCircularBar.Location = New System.Drawing.Point(0, 0)
+        Me.CPUCircularBar.Location = New System.Drawing.Point(176, 30)
         Me.CPUCircularBar.MarqueeAnimationSpeed = 2000
         Me.CPUCircularBar.Name = "CPUCircularBar"
         Me.CPUCircularBar.OuterColor = System.Drawing.Color.LightGray
         Me.CPUCircularBar.OuterMargin = -25
         Me.CPUCircularBar.OuterWidth = 26
-        Me.CPUCircularBar.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(170, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.CPUCircularBar.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(185, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(71, Byte), Integer))
         Me.CPUCircularBar.ProgressWidth = 10
         Me.CPUCircularBar.SecondaryFont = New System.Drawing.Font("Segoe UI", 10.0!)
         Me.CPUCircularBar.Size = New System.Drawing.Size(125, 125)
         Me.CPUCircularBar.StartAngle = 270
         Me.CPUCircularBar.SubscriptColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
-        Me.CPUCircularBar.SubscriptMargin = New System.Windows.Forms.Padding(-17, 10, 0, 0)
+        Me.CPUCircularBar.SubscriptMargin = New System.Windows.Forms.Padding(-22, 10, 0, 0)
         Me.CPUCircularBar.SubscriptText = "CPU"
         Me.CPUCircularBar.SuperscriptColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(166, Byte), Integer), CType(CType(166, Byte), Integer))
         Me.CPUCircularBar.SuperscriptMargin = New System.Windows.Forms.Padding(0, 35, 0, 0)
         Me.CPUCircularBar.SuperscriptText = "%"
         Me.CPUCircularBar.TabIndex = 5
         Me.CPUCircularBar.Text = "25"
-        Me.CPUCircularBar.TextMargin = New System.Windows.Forms.Padding(2, 0, 0, 0)
+        Me.CPUCircularBar.TextMargin = New System.Windows.Forms.Padding(5, 0, 0, 0)
         Me.CPUCircularBar.Value = 25
-        '
-        'Panel1
-        '
-        Me.Panel1.Controls.Add(Me.CPUCircularBar)
-        Me.Panel1.Location = New System.Drawing.Point(176, 33)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(125, 125)
-        Me.Panel1.TabIndex = 6
         '
         'CPUPerformanceCounter
         '
@@ -138,12 +130,111 @@ Partial Class NewManager
         Me.Timer.Enabled = True
         Me.Timer.Interval = 450
         '
+        'NetworkCircularBar
+        '
+        Me.NetworkCircularBar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner
+        Me.NetworkCircularBar.AnimationSpeed = 500
+        Me.NetworkCircularBar.BackColor = System.Drawing.Color.Transparent
+        Me.NetworkCircularBar.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold)
+        Me.NetworkCircularBar.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.NetworkCircularBar.InnerColor = System.Drawing.Color.White
+        Me.NetworkCircularBar.InnerMargin = 2
+        Me.NetworkCircularBar.InnerWidth = -1
+        Me.NetworkCircularBar.Location = New System.Drawing.Point(307, 30)
+        Me.NetworkCircularBar.MarqueeAnimationSpeed = 2000
+        Me.NetworkCircularBar.Name = "NetworkCircularBar"
+        Me.NetworkCircularBar.OuterColor = System.Drawing.Color.LightGray
+        Me.NetworkCircularBar.OuterMargin = -25
+        Me.NetworkCircularBar.OuterWidth = 26
+        Me.NetworkCircularBar.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(137, Byte), Integer), CType(CType(239, Byte), Integer))
+        Me.NetworkCircularBar.ProgressWidth = 10
+        Me.NetworkCircularBar.SecondaryFont = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.NetworkCircularBar.Size = New System.Drawing.Size(125, 125)
+        Me.NetworkCircularBar.StartAngle = 270
+        Me.NetworkCircularBar.SubscriptColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.NetworkCircularBar.SubscriptMargin = New System.Windows.Forms.Padding(-22, 10, 0, 0)
+        Me.NetworkCircularBar.SubscriptText = "網路"
+        Me.NetworkCircularBar.SuperscriptColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(166, Byte), Integer), CType(CType(166, Byte), Integer))
+        Me.NetworkCircularBar.SuperscriptMargin = New System.Windows.Forms.Padding(0, 35, 0, 0)
+        Me.NetworkCircularBar.SuperscriptText = "%"
+        Me.NetworkCircularBar.TabIndex = 6
+        Me.NetworkCircularBar.Text = "25"
+        Me.NetworkCircularBar.TextMargin = New System.Windows.Forms.Padding(5, 0, 0, 0)
+        Me.NetworkCircularBar.Value = 25
+        '
+        'RAMCircularBar
+        '
+        Me.RAMCircularBar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner
+        Me.RAMCircularBar.AnimationSpeed = 500
+        Me.RAMCircularBar.BackColor = System.Drawing.Color.Transparent
+        Me.RAMCircularBar.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold)
+        Me.RAMCircularBar.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.RAMCircularBar.InnerColor = System.Drawing.Color.WhiteSmoke
+        Me.RAMCircularBar.InnerMargin = 2
+        Me.RAMCircularBar.InnerWidth = -1
+        Me.RAMCircularBar.Location = New System.Drawing.Point(176, 161)
+        Me.RAMCircularBar.MarqueeAnimationSpeed = 2000
+        Me.RAMCircularBar.Name = "RAMCircularBar"
+        Me.RAMCircularBar.OuterColor = System.Drawing.Color.LightGray
+        Me.RAMCircularBar.OuterMargin = -25
+        Me.RAMCircularBar.OuterWidth = 26
+        Me.RAMCircularBar.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(153, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.RAMCircularBar.ProgressWidth = 10
+        Me.RAMCircularBar.SecondaryFont = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.RAMCircularBar.Size = New System.Drawing.Size(125, 125)
+        Me.RAMCircularBar.StartAngle = 270
+        Me.RAMCircularBar.SubscriptColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.RAMCircularBar.SubscriptMargin = New System.Windows.Forms.Padding(-20, 10, 0, 0)
+        Me.RAMCircularBar.SubscriptText = "實體記憶體"
+        Me.RAMCircularBar.SuperscriptColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(166, Byte), Integer), CType(CType(166, Byte), Integer))
+        Me.RAMCircularBar.SuperscriptMargin = New System.Windows.Forms.Padding(0, 35, 0, 0)
+        Me.RAMCircularBar.SuperscriptText = "%"
+        Me.RAMCircularBar.TabIndex = 7
+        Me.RAMCircularBar.Text = "25"
+        Me.RAMCircularBar.TextMargin = New System.Windows.Forms.Padding(15, 0, 0, 0)
+        Me.RAMCircularBar.Value = 25
+        '
+        'VRAMCircularBar
+        '
+        Me.VRAMCircularBar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner
+        Me.VRAMCircularBar.AnimationSpeed = 500
+        Me.VRAMCircularBar.BackColor = System.Drawing.Color.Transparent
+        Me.VRAMCircularBar.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold)
+        Me.VRAMCircularBar.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.VRAMCircularBar.InnerColor = System.Drawing.Color.WhiteSmoke
+        Me.VRAMCircularBar.InnerMargin = 2
+        Me.VRAMCircularBar.InnerWidth = -1
+        Me.VRAMCircularBar.Location = New System.Drawing.Point(307, 161)
+        Me.VRAMCircularBar.MarqueeAnimationSpeed = 2000
+        Me.VRAMCircularBar.Name = "VRAMCircularBar"
+        Me.VRAMCircularBar.OuterColor = System.Drawing.Color.LightGray
+        Me.VRAMCircularBar.OuterMargin = -25
+        Me.VRAMCircularBar.OuterWidth = 26
+        Me.VRAMCircularBar.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(196, Byte), Integer), CType(CType(13, Byte), Integer))
+        Me.VRAMCircularBar.ProgressWidth = 10
+        Me.VRAMCircularBar.SecondaryFont = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.VRAMCircularBar.Size = New System.Drawing.Size(125, 125)
+        Me.VRAMCircularBar.StartAngle = 270
+        Me.VRAMCircularBar.SubscriptColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.VRAMCircularBar.SubscriptMargin = New System.Windows.Forms.Padding(-20, 10, 0, 0)
+        Me.VRAMCircularBar.SubscriptText = "虛擬記憶體"
+        Me.VRAMCircularBar.SuperscriptColor = System.Drawing.Color.FromArgb(CType(CType(166, Byte), Integer), CType(CType(166, Byte), Integer), CType(CType(166, Byte), Integer))
+        Me.VRAMCircularBar.SuperscriptMargin = New System.Windows.Forms.Padding(0, 35, 0, 0)
+        Me.VRAMCircularBar.SuperscriptText = "%"
+        Me.VRAMCircularBar.TabIndex = 8
+        Me.VRAMCircularBar.Text = "25"
+        Me.VRAMCircularBar.TextMargin = New System.Windows.Forms.Padding(15, 0, 0, 0)
+        Me.VRAMCircularBar.Value = 25
+        '
         'NewManager
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.VRAMCircularBar)
+        Me.Controls.Add(Me.RAMCircularBar)
+        Me.Controls.Add(Me.NetworkCircularBar)
+        Me.Controls.Add(Me.CPUCircularBar)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.DisplayHeader = False
         Me.Name = "NewManager"
@@ -153,7 +244,6 @@ Partial Class NewManager
         Me.Text = "Minecraft 伺服器管理員"
         CType(Me.StyleManager, System.ComponentModel.ISupportInitialize).EndInit()
         Me.TableLayoutPanel1.ResumeLayout(False)
-        Me.Panel1.ResumeLayout(False)
         CType(Me.CPUPerformanceCounter, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -163,8 +253,10 @@ Partial Class NewManager
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents MetroTile2 As MetroFramework.Controls.MetroTile
     Friend WithEvents MetroTile1 As MetroFramework.Controls.MetroTile
-    Friend WithEvents Panel1 As Panel
     Friend WithEvents CPUCircularBar As CircularProgressBar.CircularProgressBar
     Friend WithEvents CPUPerformanceCounter As PerformanceCounter
     Friend WithEvents Timer As Timer
+    Friend WithEvents VRAMCircularBar As CircularProgressBar.CircularProgressBar
+    Friend WithEvents RAMCircularBar As CircularProgressBar.CircularProgressBar
+    Friend WithEvents NetworkCircularBar As CircularProgressBar.CircularProgressBar
 End Class
--- ServerManager/Redesigned GUI/NewManager.vb
+++ ServerManager/Redesigned GUI/NewManager.vb
@@ -1,10 +1,85 @@
 ﻿Public Class NewManager
+    Dim winQuery As Management.ObjectQuery = New Management.ObjectQuery("SELECT * FROM CIM_OperatingSystem")
+    Dim searcher As New Management.ManagementObjectSearcher(winQuery)
     Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
-        Dim value As Integer = CPUPerformanceCounter.NextValue
-        CPUCircularBar.Value = value
-        CPUCircularBar.Text = value
+        Dim CPUvalue As Integer = CPUPerformanceCounter.NextValue
+        CPUCircularBar.Value = CPUvalue
+        CPUCircularBar.Text = CPUvalue
+        Select Case CPUvalue
+            Case 100
+                CPUCircularBar.SubscriptMargin = New Padding(-32, 10, 0, 0)
+            Case 10 To 99
+                CPUCircularBar.SubscriptMargin = New Padding(-22, 10, 0, 0)
+            Case 0 To 9
+                CPUCircularBar.SubscriptMargin = New Padding(-12, 10, 0, 0)
+        End Select
+        For Each item In searcher.Get()
+            Try
+                Dim RAMvalue As Integer = 100 - item("FreePhysicalMemory") / item("TotalVisibleMemorySize") * 100
+                RAMCircularBar.Value = RAMvalue
+                RAMCircularBar.Text = RAMvalue
+                Select Case RAMvalue
+                    Case 100
+                        RAMCircularBar.SubscriptMargin = New Padding(-30, 10, 0, 0)
+                    Case 10 To 99
+                        RAMCircularBar.SubscriptMargin = New Padding(-20, 10, 0, 0)
+                    Case 0 To 9
+                        RAMCircularBar.SubscriptMargin = New Padding(-10, 10, 0, 0)
+                End Select
+            Catch ex As Exception
+                Continue For
+            End Try
+            Try
+                Dim VirtualRAMvalue As Integer = 100 - item("FreeVirtualMemory") / item("TotalVirtualMemorySize") * 100
+                VRAMCircularBar.Value = VirtualRAMvalue
+                VRAMCircularBar.Text = VirtualRAMvalue
+                Select Case VirtualRAMvalue
+                    Case 100
+                        VRAMCircularBar.SubscriptMargin = New Padding(-30, 10, 0, 0)
+                    Case 10 To 99
+                        VRAMCircularBar.SubscriptMargin = New Padding(-20, 10, 0, 0)
+                    Case 0 To 9
+                        VRAMCircularBar.SubscriptMargin = New Padding(-10, 10, 0, 0)
+                End Select
+            Catch ex As Exception
+                Continue For
+            End Try
+            Exit For
+        Next
+        Dim category As New PerformanceCounterCategory("Network Interface")
+        Dim names As String() = category.GetInstanceNames()
+        Dim totalValue As Double = 0
+        For Each name As String In names
+            totalValue += getNetworkUtilization(name)
+        Next
+        Console.WriteLine(totalValue)
+        Dim networkValue As Integer = totalValue / names.Count
+        NetworkCircularBar.Value = networkValue
+        NetworkCircularBar.Text = networkValue
+        Select Case networkValue
+            Case 100
+                NetworkCircularBar.SubscriptMargin = New Padding(-32, 10, 0, 0)
+            Case 10 To 99
+                NetworkCircularBar.SubscriptMargin = New Padding(-22, 10, 0, 0)
+            Case 0 To 9
+                NetworkCircularBar.SubscriptMargin = New Padding(-12, 10, 0, 0)
+        End Select
     End Sub
-
+    Public Function getNetworkUtilization(networkCard As String) As Double
+        Dim bandwidthCounter As New PerformanceCounter("Network Interface", "Current Bandwidth", networkCard)
+        bandwidthCounter.NextValue()
+        Dim bandwidth As Single = bandwidthCounter.NextValue()
+        Dim dataSentCounter As New PerformanceCounter("Network Interface", "Bytes Sent/sec", networkCard)
+        Dim dataReceivedCounter As New PerformanceCounter("Network Interface", "Bytes Received/sec", networkCard)
+        Dim sendSum As Single = 0
+        Dim receiveSum As Single = 0
+        For i As Integer = 1 To 10
+            sendSum += dataSentCounter.NextValue()
+            receiveSum += dataReceivedCounter.NextValue()
+        Next
+        Dim utilization As Double = (8 * (sendSum + receiveSum)) / (bandwidth * 10) * 100
+        Return utilization
+    End Function
     Private Sub NewManager_Load(sender As Object, e As EventArgs) Handles Me.Load
         CPUPerformanceCounter.NextValue()
     End Sub
