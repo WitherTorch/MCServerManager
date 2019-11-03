a4aae6c5bd9f6bf3d426c81759e9fc501885f8d3
Error 404 <new1271@yahoo.com.tw>
2019-11-04 04:35:57 +0800

    .

---
+++ ServerManager.Core/Classes/Task System/SkriptLoader.vb
@@ -0,0 +1,8 @@
+﻿Public Class SkriptLoader
+    Public Shared TaskActions As Dictionary(Of String, SkriptTask)
+
+End Class
+Public Structure SkriptTask
+    Public NeedArguments As String()
+    Public TaskProgram As Action(Of String())
+End Structure
