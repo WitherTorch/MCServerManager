33475c7f86d78378dbf7c1017cebbdb99d5dc221
Error 404 <new1271@yahoo.com.tw>
2019-11-30 02:00:43 +0800

    Release 1.7 Alpha 8

--- ServerManager/GlobalModule.vb
+++ ServerManager/GlobalModule.vb
@@ -7,7 +7,7 @@ Imports ServerManager
 Module GlobalModule
     Friend TestForm As ServerCheckingForm
     Public Const SERVER_MANAGER_VER As String = "1.7 Alpha 8"
-    Public Const SERVER_MANAGER_ARCH As String = "Universal"
+    Public Const SERVER_MANAGER_ARCH As String = "Windows"
     Friend Manager As Manager
     Friend IsUnixLikeSystem As Boolean
     Friend RunningBungeeCord As Boolean = False
