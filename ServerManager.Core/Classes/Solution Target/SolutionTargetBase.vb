685ca76b3b277cf5a79de35400777b2a984625ca
Error 404 <new1271@yahoo.com.tw>
2019-12-01 22:38:42 +0800

    Add SolutionTargetBase Class(W.I.P.)

---
+++ ServerManager.Core/Classes/Solution Target/SolutionTargetBase.vb
@@ -0,0 +1,44 @@
+﻿Public Class SolutionTargetBase
+#Region "Properties"
+    Dim _SolutionVersion As String
+    ''' <summary>
+    ''' 方案的目標整合器版本
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property SolutionVersion As String
+        Get
+            Return _SolutionVersion
+        End Get
+        Protected Set(value As String)
+            _SolutionVersion = value
+        End Set
+    End Property
+    Dim _SolutionPath As String
+    ''' <summary>
+    ''' 方案的資料夾路徑
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property SolutionPath As String
+        Get
+            Return _SolutionPath
+        End Get
+        Set(value As String)
+            _SolutionPath = value
+        End Set
+    End Property
+    Dim _SolutionPathName As String
+    ''' <summary>
+    ''' 方案的資料夾名稱
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property SolutionPathName As String
+        Get
+            Return _SolutionPathName
+        End Get
+        Protected Set(value As String)
+            _SolutionPathName = value
+        End Set
+    End Property
+#End Region
+
+End Class
